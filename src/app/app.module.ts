b759cdb5575189c134b7816f4e4fe2f51df042d7
WhitelightningDev <danielmommsen@hotmail.com>
2024-10-10 21:01:31 +0200

    "Updated Angular application code: added ngx-toastr, modified sign-up component, and made changes to various HTML, SCSS, and TypeScript files."

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { BurgerMenuComponent } from './burger-menu/burger-menu.component';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
-
+import { FormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent,BurgerMenuComponent,SignUpComponent,LoginComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule,ToastrModule.forRoot()],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,14 +1,41 @@
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+// sign-up.component.ts
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr'; // Import ToastrService
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
 })
-export class SignUpComponent  implements OnInit {
+export class SignUpComponent implements OnInit {
+[x: string]: any;
+  formData = {
+    name: '',
+    surname: '',
+    contact: '',
+    email: '',
+    password: '',
+    privacyPolicy: false,
+    termsConditions: false,
+    agreement: false,
+  };
 
-  constructor() { }
+  constructor(private toastr: ToastrService) {} // Inject ToastrService
 
   ngOnInit() {}
 
+  onSubmit(form: any) {
+    // Check for form validity
+    if (form.valid) {
+      // Handle successful form submission
+      console.log('Form submitted successfully:', this.formData);
+      this.toastr.success('Sign Up successful!', 'Success'); // Show success toast
+      // Reset the form after submission
+      form.reset();
+    } else {
+      // Display a message indicating which fields are missing
+      this.toastr.error('Please fill in all required fields and agree to the terms.', 'Error'); // Show error toast
+    }
+  }
 }
